1ef822d5b4c321400af9db36c2819ca49a506c62
Iunius118 <18502177+Iunius118@users.noreply.github.com>
2023-12-03 17:52:03 +0900

    Add config to change item ID

--- src/main/java/com/github/iunius118/tolaserblade/ToLaserBlade.java
+++ src/main/java/com/github/iunius118/tolaserblade/ToLaserBlade.java
@@ -3,11 +3,24 @@ package com.github.iunius118.tolaserblade;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import turniplabs.halplibe.util.ConfigHandler;
+import java.util.Properties;
 
 
 public class ToLaserBlade implements ModInitializer {
     public static final String MOD_ID = "tolaserblade";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	public static final int STARTING_ITEM_ID;
+
+	static {
+		// Handle config
+		Properties prop = new Properties();
+		prop.setProperty("starting_item_id","24530");
+		ConfigHandler config = new ConfigHandler(MOD_ID, prop);
+		STARTING_ITEM_ID = config.getInt("starting_item_id");
+		config.updateConfig();
+	}
 
     @Override
     public void onInitialize() {
