98a74ea09922d33e68ff67cab2ca650a03bb65f5
Iunius118 <18502177+Iunius118@users.noreply.github.com>
2023-12-03 17:55:46 +0900

    Fix tabs in code

--- src/main/java/com/github/iunius118/tolaserblade/ToLaserBlade.java
+++ src/main/java/com/github/iunius118/tolaserblade/ToLaserBlade.java
@@ -13,15 +13,15 @@ import java.util.Properties;
 
 
 public class ToLaserBlade implements ModInitializer {
-    public static final String MOD_ID = "tolaserblade";
-    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	public static final String MOD_ID = "tolaserblade";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	public static final int STARTING_ITEM_ID;
 
 	static {
 		// Handle config
 		Properties prop = new Properties();
-		prop.setProperty("starting_item_id","24530");
+		prop.setProperty("starting_item_id", "24530");
 		ConfigHandler config = new ConfigHandler(MOD_ID, prop);
 		STARTING_ITEM_ID = config.getInt("starting_item_id");
 		config.updateConfig();
@@ -30,17 +30,17 @@ public class ToLaserBlade implements ModInitializer {
 	public static final ToolMaterial LASER_MATERIAL = new ToolMaterial().setDurability(0).setEfficiency(7.0F, 14.0F).setMiningLevel(3).setDamage(4).setBlockHitDelay(4);
 	public static final Item LB_SWORD = ItemHelper.createItem(MOD_ID, new ItemLBSword("laser_blade", STARTING_ITEM_ID, LASER_MATERIAL), "laser_blade", "laser_blade.png");
 
-    @Override
-    public void onInitialize() {
-	    RecipeHelper.Crafting.createRecipe(LB_SWORD, 1, new Object[]{
+	@Override
+	public void onInitialize() {
+		RecipeHelper.Crafting.createRecipe(LB_SWORD, 1, new Object[]{
 			" id",
-		    "igi",
-		    "ri ",
-		    'i', Item.ingotIron,
-		    'd', Item.diamond,
-		    'q', Item.dustGlowstone,
-		    'r', Item.dustRedstone});
+			"igi",
+			"ri ",
+			'i', Item.ingotIron,
+			'd', Item.diamond,
+			'q', Item.dustGlowstone,
+			'r', Item.dustRedstone});
 
-	    LOGGER.info("ToLaserBlade initialized.");
-    }
+		LOGGER.info("ToLaserBlade initialized.");
+	}
 }
