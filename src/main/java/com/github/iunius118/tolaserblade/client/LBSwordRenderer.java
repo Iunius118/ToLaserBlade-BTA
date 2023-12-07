394c6066fdefbc90c728463e11ae5e3312ddff39
Iunius118 <18502177+Iunius118@users.noreply.github.com>
2023-12-07 22:16:34 +0900

    Add laser blade renderer

---
+++ src/main/java/com/github/iunius118/tolaserblade/client/LBSwordRenderer.java
@@ -0,0 +1,89 @@
+package com.github.iunius118.tolaserblade.client;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiPhotoMode;
+import net.minecraft.client.render.Tessellator;
+import net.minecraft.client.render.camera.EntityCamera;
+import net.minecraft.core.entity.Entity;
+import net.minecraft.core.item.ItemStack;
+import net.minecraft.core.util.helper.MathHelper;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL14;
+import org.lwjgl.util.vector.Vector4f;
+
+import java.util.List;
+
+public class LBSwordRenderer {
+	public void doRender(Entity entity, ItemStack itemstack, boolean handheldTransform) {
+		Minecraft mc = Minecraft.getMinecraft(Minecraft.class);
+		Tessellator tessellator = Tessellator.instance;
+
+		GL11.glEnable(GL11.GL_CULL_FACE);
+		GL11.glDisable(GL11.GL_TEXTURE_2D);
+
+		// Transform for laser blade model
+		GL11.glTranslatef(0.858579F, 0.141421F, -0.0234375F);
+		// The following is equal to the right: GL11.glRotatef(45.0F, 0.0F, 0.0F, 1.0F); GL11.glRotatef(-95.0F, 0.0F, 1.0F, 0.0F);
+		GL11.glRotatef(102.753013F, 0.361063F, -0.871876F, 0.330857F);
+		// Fix scaling in third person
+		if (!isRenderingItemInFirstPerson(mc)) {
+			GL11.glScalef(-1.0F, 1.0F, 1.0F);
+		}
+
+		// Render hilt
+		renderHilt(tessellator, getBrightness(mc, entity));
+
+		// Change render mode for laser blades
+		GL11.glDisable(GL11.GL_LIGHTING);
+		GL11.glEnable(GL11.GL_BLEND);
+		GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE);
+		//GL14.glBlendEquation(GL14.GL_FUNC_REVERSE_SUBTRACT);
+
+		// Render blades
+		renderBlade(tessellator, LBSwordModel.BLADE_OUT_QUADS, new Vector4f(1.0F, 0.0F, 0.0F, LBSwordModel.BLADE_OUT_COLOR.w));
+		renderBlade(tessellator, LBSwordModel.BLADE_MID_QUADS, new Vector4f(1.0F, 0.0F, 0.0F, LBSwordModel.BLADE_MID_COLOR.w));
+		renderBlade(tessellator, LBSwordModel.BLADE_IN_QUADS, LBSwordModel.BLADE_IN_COLOR);
+
+		// Restore render settings
+		GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+		GL14.glBlendEquation(GL14.GL_FUNC_ADD);
+		GL11.glDisable(GL11.GL_BLEND);
+		GL11.glEnable(GL11.GL_LIGHTING);
+		GL11.glEnable(GL11.GL_TEXTURE_2D);
+		GL11.glDisable(GL11.GL_CULL_FACE);
+	}
+
+	private boolean isRenderingItemInFirstPerson(Minecraft mc) {
+        return mc.gameSettings.thirdPersonView.value == 0
+                && !(mc.currentScreen instanceof GuiPhotoMode)
+                && mc.activeCamera instanceof EntityCamera
+                && !((EntityCamera) mc.activeCamera).entity.isPlayerSleeping()
+                && mc.gameSettings.immersiveMode.drawHand()
+                && (!mc.gameSettings.keyZoom.isPressed() || mc.currentScreen != null);
+    }
+
+	private float getBrightness(Minecraft mc, Entity entity) {
+		if (mc.fullbright) {
+			return 1.0F;
+		} else {
+			return mc.theWorld.getLightBrightness(MathHelper.floor_double(entity.x), MathHelper.floor_double(entity.y), MathHelper.floor_double(entity.z));
+		}
+	}
+
+	private void renderHilt(Tessellator tessellator, float brightness) {
+		Vector4f color = LBSwordModel.Hilt_COLOR;
+		GL11.glColor4f(color.x * brightness, color.y * brightness, color.z * brightness, color.w);
+
+		for (SimpleQuad quad : LBSwordModel.HILT_QUADS) {
+			quad.renderQuad(tessellator);
+		}
+	}
+
+	private void renderBlade(Tessellator tessellator, List<SimpleQuad> quads, Vector4f color) {
+		GL11.glColor4f(color.x, color.y, color.z, color.w);
+
+		for (SimpleQuad quad : quads) {
+			quad.renderQuad(tessellator);
+		}
+	}
+}
--- src/main/java/com/github/iunius118/tolaserblade/mixin/MixinItemRender.java
+++ src/main/java/com/github/iunius118/tolaserblade/mixin/MixinItemRender.java
@@ -1,7 +1,7 @@
 package com.github.iunius118.tolaserblade.mixin;
 
 import com.github.iunius118.tolaserblade.ToLaserBlade;
-import com.github.iunius118.tolaserblade.client.LaserBladeRenderer;
+import com.github.iunius118.tolaserblade.client.LBSwordRenderer;
 import net.minecraft.client.render.ItemRenderer;
 import net.minecraft.client.render.RenderBlocks;
 import net.minecraft.client.render.block.model.BlockModelRenderBlocks;
@@ -10,6 +10,7 @@ import net.minecraft.core.item.ItemStack;
 import org.lwjgl.opengl.GL11;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -18,6 +19,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class MixinItemRender {
 	@Shadow
 	private RenderBlocks renderBlocksInstance;
+
+	@Unique
+	private LBSwordRenderer lBSwordRenderer = new LBSwordRenderer();
 
 	@Inject(method = "renderItem(Lnet/minecraft/core/entity/Entity;Lnet/minecraft/core/item/ItemStack;Z)V", at = @At("HEAD"), cancellable = true)
 	private void onRenderItem(Entity entity, ItemStack itemstack, boolean handheldTransform, CallbackInfo ci) {
@@ -34,7 +38,7 @@ public class MixinItemRender {
 				GL11.glTranslatef(-0.9375F, -0.0625F, 0.0F);
 			}
 
-			LaserBladeRenderer.doRender(entity, itemstack, handheldTransform);
+			lBSwordRenderer.doRender(entity, itemstack, handheldTransform);
 			GL11.glDisable(32826);
 			GL11.glPopMatrix();
 
